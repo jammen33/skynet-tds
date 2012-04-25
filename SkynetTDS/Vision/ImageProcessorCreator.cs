a4d2f4535e80c8554363ec3cbcae42ce053e7cfa
joe.carter <jammen@gmail.com>
2012-04-24 23:22:55 -0700

    added interfaces for imageprocess creators

---
+++ SkynetTDS/Vision/ImageProcessorCreator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkynetTDS.Vision
+{
+    class ImageProcessorCreator : IImageProcessorCreator
+    {
+        public IImageProcessor createProcessor(string type)
+        {
+            switch (type)
+            {
+                case "foe":
+                    return new FoeImageProcessor();
+                case "friendFoe":
+                    return new FriendFoeImageProcessor();
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
